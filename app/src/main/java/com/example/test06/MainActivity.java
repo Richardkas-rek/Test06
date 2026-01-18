6c4ebaa0ac861b53efb46dc094fe91831df405c6
Richardkas-rek <richardlkas161210@gmail.com>
2026-01-18 17:39:13 +0200

    java redacktor

--- app/src/main/java/com/example/test06/MainActivity.java
+++ app/src/main/java/com/example/test06/MainActivity.java
@@ -1,20 +1,40 @@
 package com.example.test06;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView iVone;
+    private Button nbgf;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        iVone = findViewById(R.id.iVone);
+        nbgf = findViewById(R.id.nbgf);
     }
+
+    int x = 1;
+
+    public void clicked(View view) {
+        x = (int) (Math.random() * 3 + 1);
+
+        nbgf.setText(String.valueOf(x));
+
+        if (x == 1) {
+            iVone.setImageResource(R.drawable.commitandpush);
+        } else if (x == 2) {
+            iVone.setImageResource(R.drawable.lil);
+        } else {
+            iVone.setImageResource(R.drawable.sum);
+        }
+    }
+
 }
+
+
